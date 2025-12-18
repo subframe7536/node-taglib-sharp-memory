61cff648289b03a768f27d367aee21b46c1ca57c
Song <1667077010@qq.com>
2025-12-18 10:18:16 +0800

    Adjust genre value splitting in textInformationFrame
    Modified genre value splitting logic to avoid trimming.

--- src/id3v2/frames/textInformationFrame.ts
+++ src/id3v2/frames/textInformationFrame.ts
@@ -461,7 +461,8 @@ export class TextInformationFrame extends Frame {
                 if (value.length > 0) {
                     // Split the remaining genre value by dividers if the setting is turned on
                     let splitValue = Id3v2Settings.useNonStandardV2V3GenreSeparators
-                        ? value.split(/[\/;]/).map((v) => v.trim()).filter((v) => !!v)
+                        // Don't trim here: https://github.com/benrr101/node-taglib-sharp/pull/121
+                        ? value.split(/[\/;]/).filter((v) => !!v)
                         : [value];
 
                     splitValue = splitValue.map((v) => {
